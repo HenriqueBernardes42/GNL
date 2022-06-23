124f9ee5c6fb455b69c47ed72afdc04b86fe6285
hbernard <hbernard@student.42sp.org.br>
2022-06-24 01:57:06 +0200

    fazendo push

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: hbernard <hbernard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 23:21:24 by hbernard          #+#    #+#             */
-/*   Updated: 2022/06/23 06:23:33 by hbernard         ###   ########.fr       */
+/*   Updated: 2022/06/24 01:48:35 by hbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,4 @@
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *str, char *buffer);
 size_t	ft_strlen(char *s);
-char	*ft_get_line(char *left_str);
-char	*ft_new_left_str(char *left_str);
 #endif
